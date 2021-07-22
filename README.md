56690735a9061970539fd70158ba5c3efd2041a1
Amish Fakun <amishfakun20@gmail.com>
2021-07-22 18:35:04 +0400

    second commit

--- README.md
+++ README.md
@@ -1,18 +1,1 @@
-## Weather App
-
-## App Description
-I am building a weather app as a personal side project to gain more experience using HTML, CSS and Javascript. The main learning goal for this is to gain familiarity with using APIs. 
-
-The end product should look a little like this:
-
-1) The user should be able to type a city name and get the weather.
-2) The app will request permission to get user's location to provide weather in current location. If not possible, an error message will be displayed.
-
-
-### Commit 1
-Built the main interface using a card interface, a search bar and a few basic weather details using HTML and CSS. So far this app is static (See gif) - Waiting on API key from Open weather to get validated. 
-
-### App Walk-though - commit 1
-<img src="https://github.com/amishfakun/weatherApp/blob/main/weatherApp1.gif" width=250><br>
-
-
+# weatherApp
--- app.js
+++ app.js
@@ -1,4 +1,33 @@
 let weather = {
-    "apiKey" : "64b161c71576569e995f3f0a42d1a554"
+    "apiKey" : "c930dbb5749bf003a6893525d06be1d4",
+    fetchWeather: function(city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" +
+              city +
+              "&units=metric&appid=" +
+              this.apiKey
+          )
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+    
 
-}
+
+    },
+
+
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name,icon,description,temp,humidity,speed)
+
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src =   "https://openweathermap.org/img/wn/" + icon + ".png";
+
+
+
+
+    }
+
+};
