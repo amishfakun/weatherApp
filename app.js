3d965f4ca0ffd5e4ca2f8d20008a1677ef941d13
Amish Fakun <amishfakun20@gmail.com>
2021-07-22 19:17:29 +0400

    final commit

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ let weather = {
     },
   };
   
-  document.querySelector(".search-button").addEventListener("click", function () {
+  document.querySelector(".search button").addEventListener("click", function () {
     weather.search();
   });
   
